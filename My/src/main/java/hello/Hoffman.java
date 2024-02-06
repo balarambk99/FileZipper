f699410c951ebe63086545f9634e9d8858259f80
balarambk99 <balarambk99@gmail.com>
2024-02-06 23:19:17 +0530

    sucessfull

---
+++ My/src/main/java/hello/Hoffman.java
@@ -0,0 +1,321 @@
+package hello;
+
+import java.util.*;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
+import java.awt.Point;
+import java.io.*;
+@WebServlet("/hoff")
+@MultipartConfig
+public class Hoffman  extends HttpServlet {
+	static HashMap <Character,String>newcode=new HashMap<>();
+	 static HashMap <String,Character>code=new HashMap<>();
+	
+	public void doGet(HttpServletRequest request,HttpServletResponse response)
+	{PrintWriter po=null;
+		try { 
+	po=response.getWriter();
+		po.println("hello::");
+		doPost(request, response);
+		po.println("hello-entde");
+		}catch (Exception e) {
+			po.println("error ::hello"+e.getCause()+e.getMessage());
+			// TODO: handle exception
+		}
+	}
+	
+	public void doPost(HttpServletRequest request,HttpServletResponse response)
+	{  PrintWriter po=null;
+		try{
+			
+			Part part=request.getPart("file");
+		InputStream p=part.getInputStream();
+		
+		InputStream fi=p;	
+				
+		int a=0;
+		String aString=request.getParameter("name");
+	
+			ServletContext context=getServletContext(); 
+                     String mt=null;
+		 response.setContentType("application/octet-stream");
+
+		  System.out.print("hello ");
+		 
+			
+		//byte aa[]=v.readAllBytes();
+		 String hkString="Context-Disposition";
+		 String g=String.format("attachement;filname=\"%s\"", aString);
+		  context.setAttribute("my","hello");	
+		 
+	  	OutputStream o=response.getOutputStream();
+	  	File kFile=compressFile(p);
+		FileInputStream v=new NewFile("C:\\Program Files\\Apache Software Foundation\\Tomcat 10.1\\work\\balaram.jar");
+		 response.setHeader(hkString, g);
+		response.setContentLength(v.available());
+	  	ObjectOutputStream objectOutputStream=new ObjectOutputStream(o);
+	  	objectOutputStream.writeObject(v);
+	  objectOutputStream.writeObject(code);
+	
+	  	//System.out.print(code);
+	  	int c=0;
+	  	StringBuffer buffer=new StringBuffer();
+//	  	while((c=v.read())!=-1)
+//	  			{o.write(c);
+//	  			  String aa=Integer.toBinaryString(c);
+//	  			  if(aa.length()!=8)
+//	  			  {
+//	  				  for(int i=0;i<=8;i++)
+//	  				  {
+//	  					  aa="0"+aa;
+//	  				  }
+//	  			  }
+//	  			  buffer.append(a);
+//	  			}
+	  	String l="";
+	  			for(int i=0;i<buffer.length();i++)
+	  			{
+	  				l+=buffer.charAt(i);
+	  				if(code.get(l)!=null)
+	  				{
+	  					System.out.print(code.get(l));
+	  					l="";
+	  				}
+	  			}
+	  				
+
+		 }catch (Exception e) 
+		{
+			e.printStackTrace();
+		 }
+		
+			
+			
+			
+	}
+	 File compressFile(InputStream p)
+	{ InputStream v=null;
+	  FileOutputStream fi=null;
+		try {
+			fi=new FileOutputStream("C:\\Program Files\\Apache Software Foundation\\Tomcat 10.1\\work\\myname.java");
+			
+			int a=0;
+	          HashMap<Integer,Integer> h=new HashMap<>();
+	          while((a=p.read())!=-1)
+	          {
+	            
+	            fi.write(a);
+	            if(h.get(a)!=null)
+	             { h.put(a,h.get(a)+1);
+	       
+	             }
+	            else  
+	              h.put(a,1);
+	          }
+	          a=32;
+	        	System.out.print(newcode);
+	         // System.out.print("happ:"+h);
+	         // System.out.print("my code"+newcode+" "+code);
+	          v=new FileInputStream("C:\\Program Files\\Apache Software Foundation\\Tomcat 10.1\\work\\myname.java");
+				
+	           hoffman(h);
+		}catch(Exception e)
+		{  
+			String aString=e.getMessage();
+			
+		}
+		finally{
+			
+//			
+			return compress(v);
+			
+		}
+	}
+	
+	 public static void hoffman(HashMap<Integer,Integer>h)
+	    {
+	      PriorityQueue<Node>q=new PriorityQueue<>(5,new ComNode());
+	          Set<Integer> s=h.keySet();
+	          for(int aa:s)
+	          {
+	            q.add(new Node((char)aa, h.get(aa)));
+	          }
+	        
+	        Node myTree=myTree(q);
+	        myCode(myTree,"");
+	    }
+	
+	 public static void myCode(Node tree,String a)
+	    {  if(tree==null)
+	         {  
+	          return;
+	         }
+	         else if(tree.left==null&&tree.right==null)
+	           {
+	        	 code.put(a,tree.a); 
+	            newcode.put(tree.a,a);
+
+	        }
+	       myCode(tree.left,a+"0");
+	       myCode(tree.right, a+"1");
+
+	    }
+
+	 public static void decompress()
+	    {  FileInputStream fi=null;
+	      FileOutputStream h=null;
+	 
+	      try
+	      {
+	       	        h=new FileOutputStream("C:\\Users\\balar\\OneDrive\\Desktop\\myname.txt");
+	        int a=0;	       
+	        StringBuffer s=new StringBuffer();
+	        while((a=fi.read())!=-1)
+	        {  String x=Integer.toBinaryString(a);
+	          for(int i=x.length();i<8;i++)
+	            x="0"+x;
+	          s.append(x);
+	         
+	          
+	        }
+	   
+	        String sa="";
+	  
+	        for(int i=0;i<s.length();i++)
+	        {   sa=sa+s.charAt(i);
+	       
+	              if(code.get(sa)!=null)
+	              {char l=code.get(sa);
+	                
+	                h.write(l);
+	                sa="";
+	              }
+	        }
+	        
+	        
+	       
+	      }catch(Exception e)
+	      {
+	       e.printStackTrace();
+	      }
+
+
+
+	    }
+	 static  File compress(InputStream l)
+     {
+    
+		 FileOutputStream fi=null;
+      
+       try{
+         fi= new FileOutputStream("C:\\Program Files\\Apache Software Foundation\\Tomcat 10.1\\work\\balaram.jar");
+           int a=0;
+           StringBuffer str=new StringBuffer();
+           while((a=l.read())!=-1)
+           {   
+            str.append(newcode.get((char)a));
+           }
+           ;
+           StringBuffer fB=new StringBuffer();
+        for(int i=0;i<str.length();i=i+8)
+           {  try{
+            int k=Math.min(i+8,str.length());
+           String su=str.substring(i, k);
+          int y=Integer.parseInt(su,2);
+          fi.write((char)y);
+          fB.append(su);
+           }
+     catch(NumberFormatException e)
+         { e.printStackTrace();
+         }
+
+        }
+        // p.print("scess:<br>"+str);
+        }
+       catch(Exception e)
+     {
+       e.printStackTrace();
+     }
+       finally {
+    	    try{FileInputStream f=new FileInputStream("C:\\Program Files\\Apache Software Foundation\\Tomcat 10.1\\work\\balaram.jar");
+    	     int a=0;
+    	     StringBuilder s=new StringBuilder();
+    	     
+//    	     while((a=f.read())!=-1)
+//    	     {
+//    	    	 s.append(a);
+//    	     }
+    	    
+    	    }catch(Exception e)
+    	    {
+    	    	e.printStackTrace();
+    	    }
+    	    
+    	   File gFile=new File("C:\\Program Files\\Apache Software Foundation\\Tomcat 10.1\\work\\balaram.jar");
+		return gFile;
+	}
+     }
+
+	
+	public static Node myTree(PriorityQueue <Node>a)
+    {
+        while(a.size()>=2&&a.size()!=0)
+        {
+          Node leftNode=a.poll();
+          Node rightNode=a.poll();
+          Node root=new Node(leftNode.b+rightNode.b,leftNode,rightNode);
+          a.add(root);
+
+        }
+        return a.poll();
+    }
+
+}
+class Node{
+	  char a;
+	  int b;
+	  Node left;
+	  Node right;
+	  public Node(int b,Node left,Node right)
+	  {
+	    this.b=b;
+	    this.left=left;
+	    this.right=right;
+	  }
+	  public Node(char a,int b)
+	  {
+	    this.a=a;
+	    this.b=b;
+	    this.left=null;
+	    this.right=null;
+	  }
+
+	}
+class NewFile extends FileInputStream implements Serializable
+{
+	public NewFile(String a) throws IOException 
+	{
+		super(a);
+	}
+}
+
+	class ComNode implements Comparator<Node>{
+	  public int compare(Node s1, Node s2) {
+	                if (s1.b < s2.b)
+	                    return -1;
+	                else if (s1.b> s2.b)
+	                    return 1;
+	                                return 0;
+	                }
+	
+
+}
+	
---
+++ My/src/main/webapp/index.jsp
@@ -0,0 +1,122 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" import="java.io.*"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet" href="./me.css"></link>
+<style> 
+@media (max-width:12000px)and (min-width:600px){
+.container{
+margin-top:100px;
+width:600px;
+
+}
+}
+
+#he{
+  display:none;
+}
+.#download ,.container{
+margin-top:50px;
+background-color:
+border-style:solid;
+border-width:3px;
+border-radius:10px;
+
+}
+#dec{
+display:none;
+}
+ .container:hover{
+ border-width:5px;
+border-color:red;
+border-shadow:rgba(0, 0, 0, 0.3);
+
+}#formFileLg{
+width:300px;
+}
+.bg-danger{
+--bs-bg-opacity: .9;
+ckground-color: rgba(var(--bs-success-rgb), var(--bs-bg-opacity));
+}
+</style>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+  <head></head>
+<center>
+<head><button onclick="swit('up')">Compress</button><button onclick="swit('dec')">Decompress</button></head>
+<div  class="container bg-danger"  id="up">
+<form action="hoff"  method="post" enctype="multipart/form-data">
+ <div class="w-100 mx-0">
+                        <h1>Compress File</h1>
+                        <h2 class="text-left">Use this online file compressor to reduce the file size of your file documents. Make file smaller to send them via e-mail or upload them to the internet.</h2>
+                    </div>
+       <div class="row"><div><input class="form-control form-control-lg col" id="formFileLg" name="name"></input></div><div class="col"></div>
+  </div>              
+    <div class="row"><div><input class="form-control form-control-lg col" id="formFileLg" name="file" type="file">   </input></div><div class="col"><i class="bi bi-cloud-arrow-up-fill"></i></div>
+  </div>
+   <button name="sub" value="upload" type="submit" class="btn btn-primary mb-3">Submit File</button>
+ 
+    <div class="col-auto">
+     </div>
+     </i>
+</form>
+</div>
+
+<div class="container bg-danger" id="dec">
+   <form action="Decompress" metho="post">
+   <div class="w-100 mx-0">
+                        <h1>Decompress File</h1>
+                        <h2 class="text-left">Use this online file compressor to reduce the file size of your file documents. Make file smaller to send them via e-mail or upload them to the internet.</h2>
+                    </div>
+                    
+    <div class="row"><div><input class="form-control form-control-lg col" id="formFileLg" name="file" type="file">   </input></div><div class="col"><i class="bi bi-cloud-arrow-up-fill"></i></div>
+  </div>
+  
+   <button name="sub" value="upload" type="submit" class="btn btn-primary mb-3">Submit File</button>
+ 
+    <div class="col-auto">
+     </div>
+     </i> 
+   </form>
+</div>
+ 
+<!--  
+<div  class="container" id="download">
+  <form action="hoff" method="post">
+    <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Enter File Name</label>
+    <input type="text" class="form-control" id="exampleInputPassword1">
+  </div>
+  <button name="download" type="submit" class="btn btn-primary"></i>download</button>
+ </form>
+  
+</div>
+
+<button onclick="he()">click </button>-->
+
+
+<script>
+ function swit(a)
+    { 
+	 if(a=='dec')
+		 {
+		 document.getElementById("dec").style.display="block";
+		 document.getElementById("up").style.display="none";
+		 
+		 }
+	 else{
+		 document.getElementById("dec").style.display="none";
+		 document.getElementById("up").style.display="block";
+		 
+		 
+	 }
+	 
+     }
+</script>
+</body>
+
+</html>
